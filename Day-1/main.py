abcb6be8e401580c6f3a32eecf4e27a8d17012f4
Aditya Raj <aditya7raj1@gmail.com>
2023-10-05 14:09:38 +0530

    learnt something

--- Day-1/main.py
+++ Day-1/main.py
@@ -1,1 +1,24 @@
-print("Hello World")
+# Printing to the console in Python 🐍
+
+print("Hello World!")
+
+print("----")
+
+# Exercise 1 - Printing
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("----")
+
+# Exercise 2 - Debugging Practice
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+# print("e.g. print("Hello " + "world")")  # this is wrong, correct line is below
+print("e.g. print('Hello ' + 'World')")
+print("New lines can be created with a backslash and n")
+
+
+print("----")
